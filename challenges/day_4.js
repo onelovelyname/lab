e266a1e9daefe475908a4e756d82a59f23e97070
mahd18-meet <mahd18@meet.mit.edu>
2019-03-22 14:04:53 +0200

    Added day 4 challenge

---
+++ challenges/day_4.js
@@ -0,0 +1,13 @@
+// Warm Up Challenge for day 4!
+// Create an array of numbers, log every item less than 5.(Make sure to create numbers both larger or less than 5)
+// For Practice, do this inside a function
+// Your code here:
+
+
+
+
+
+
+
+// Extra Credit: use .forEach to complete this
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
