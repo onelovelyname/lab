ea3a4c157d3f9d06e643e06b912a9e349ca016da
mahd18-meet <mahd18@meet.mit.edu>
2019-03-22 15:11:10 +0200

    Added day4 challenge+updated syntax cheat sheet

---
+++ challenges/day_4.js
@@ -0,0 +1,40 @@
+<<<<<<< HEAD
+// Warm Up Challenge for day 4
+// Create an array of numbers
+// Create a functions that returns a new array, with only numbers less than 5 from the previous array
+=======
+// Warm Up Challenge for day 4!
+// Create an array of numbers, create a functions that returns a new array, with only numbers less than 5
+// For Practice, do this inside a function
+>>>>>>> 5a5e04e70e592c9e6d1620f21415bb3cd758fdf9
+// Your code here:
+
+
+
+
+
+
+
+// Extra Credit: use .forEach to complete this
+<<<<<<< HEAD
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+// Your code here:
+
+
+
+
+
+
+
+
+
+// Extra Extra Credit: Use .filter to complete this
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+// Your code here:
+
+
+
+
+=======
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+>>>>>>> 5a5e04e70e592c9e6d1620f21415bb3cd758fdf9
